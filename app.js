521eebf2a6bcec36f02e27f5c96a0217e07ccbc8
Camilo Segura <camilosegurar@gmail.com>
2017-10-10 19:31:02 -0500

    feat: Added basic routes and pages.  Added handlebars

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
+const exphbs  = require('express-handlebars');
 const port = 3000;
 
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', (req, res) => {
+  const title = 'Index Page';
+  res.render('index', {
+    title: title
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
