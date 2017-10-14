4c4ec1189e6a09c5c5a36ae889e7595e23ed89e4
Camilo Segura <camilosegurar@gmail.com>
2017-10-14 13:10:26 -0500

    feat: stored new ideas and show an ideas list

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('./models/Idea');
 const port = 3000;
@@ -24,6 +25,10 @@ app.engine('.hbs', exphbs({
 }));
 app.set('view engine', '.hbs');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   const title = 'Index Page';
   res.render('index', {
@@ -38,6 +43,49 @@ app.get('/about', (req, res) => {
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
 });
+
+app.post('/ideas', (req, res) => {
+  const errors = [];
+
+  if (!req.body.title) {
+    errors.push({text: 'Please add a title'});
+  }
+
+  if (!req.body.details) {
+    errors.push({text: 'Please add some details'});
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      erros: erros,
+      title: req.body.title,
+      details: req.body.details
+    })
+  } else {
+    const newUser = {
+      title: req.body.title,
+      details: req.body.details
+    }
+    new Idea(newUser)
+      .save()
+      .then(() => {
+        res.redirect('/ideas');
+      });
+  }
+});
+
+app.get('/ideas', (req, res) => {
+  Idea.find({})
+    .sort({
+      date: 'desc'
+    })
+    .then(ideas => {
+      res.render('ideas/index', {
+        ideas: ideas
+      });
+    })
+
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
