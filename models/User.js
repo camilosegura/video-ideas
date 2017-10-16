b32e235b32798fc5684fc49ca74bf43d7966ef4a
Camilo Segura <camilosegurar@gmail.com>
2017-10-15 19:53:44 -0500

    feat: added register users model and logic

--- models/User.js
+++ models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  title: {
-    name: String,
+  name: {
+    type: String,
     required: true
   },
   email: {
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// const passport = require('passport');
+const router = express.Router();
+
+require('../models/User');
+
+const User = mongoose.model('users');
+
 
 router.get('/login', (req, res) => {
   res.render('users/login');
@@ -25,6 +32,37 @@ router.post('/register', (req, res) => {
       password: req.body.password,
       password2: req.body.password2
     });
+  } else {
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if (user) {
+          req.flash('error_msg', 'Email is already registered')
+          res.redirect('/users/register');
+        } else {
+          const newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+          }
+
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              newUser.password = hash;
+
+              new User(newUser)
+                .save()
+                .then(() => {
+                  req.flash('success_msg', 'You are registerd!');
+                  res.redirect('/users/login');
+                })
+                .catch(err => {
+                  console.log(err);
+                  return;
+                }) ;
+            });
+          });
+        }
+      });
   }
 });
 
