feaf0b4080a12cdd6319c2db072dbac0132e0a93
Camilo Segura <camilosegurar@gmail.com>
2017-10-17 15:17:57 -0500

    feat: protecting routes

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const { ensureAuthenticated } = require('../helpers/auth');
 
 require('../models/Idea');
 
 const Idea = mongoose.model('ideas');
 
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
   res.render('ideas/add');
 });
 
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
   const errors = [];
 
   if (!req.body.title) {
@@ -53,7 +54,7 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   })
@@ -64,7 +65,7 @@ router.get('/edit/:id', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
   Idea.findOne({
     _id: req.params.id
   })
@@ -80,7 +81,7 @@ router.put('/:id', (req, res) => {
    });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
   Idea.remove({
     _id: req.params.id
   })
