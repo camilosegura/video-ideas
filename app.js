037d0d95627c58b26939638d74dd12c223409943
Camilo Segura <camilosegurar@gmail.com>
2017-10-17 16:41:55 -0500

    debug: debbugin production env var

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const db = require('./config/database');
 
 require('./config/passport')(passport);
 
-console.log('server vars', process.env.port, db)
+console.log('server vars', process.env.port, db, process.env)
 mongoose.Promise = global.Promise;
 
 mongoose.connect(db.mongoURI, {
